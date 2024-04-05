117372bc462602d24514c6cc95e67c80f47edd2d
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-04-05 01:31:48 +0000

    Entrega

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+console.log("entre al archivo");
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("entre a la funcion");
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "K", "Q"];
+  let palos = ["♦", "♥", "♠", "♣"];
+  let randomElements = function(array) {
+    let aleatorio = array[Math.floor(Math.random() * array.length)];
+    console.log(aleatorio);
+    return aleatorio;
+  };
+  let paloArr = document.getElementById("paloArriba");
+  let paloAba = document.getElementById("paloAbajo");
+  let valor = document.getElementById("valor");
+  let palosAleatorios = randomElements(palos);
+
+  paloArr.innerText = palosAleatorios;
+  paloAba.innerText = palosAleatorios;
+  valor.innerText = randomElements(numeros);
+  let corazon = "♥";
+  let diamante = "♦";
+  if (palosAleatorios == corazon || palosAleatorios == diamante) {
+    paloArr.style.color = "red";
+    paloAba.style.color = "red";
+  }
 };
